a4f40e3db35280b957ce515b53727feb8c4d3ed0
Abdullah <abdullahwazid31006@gmail.com>
2026-07-02 01:35:20 +0530

    Code for the Game

--- game.py
+++ game.py
@@ -1,1 +1,39 @@
-import random
+import random #Library for random choices for the computer
+
+options = ["ROCK", "PAPER", "SCISSORS"];
+
+while True : #Outer Loop for Replay
+
+    while True : #Inner Loop for checking valid Input
+        player = input("Enter Your Choice(Rock, Paper or Scissors) : ").upper()
+    
+        if player not in options : 
+            print("Invalid Choice! Please Try Again.\n")
+            continue 
+
+        break
+
+    computer = random.choice(options)
+    print("You Chose : ", player)
+    print("Computer Chose : ", computer)
+
+    if player == computer : 
+        print("Match Tied!")
+    elif(player == "ROCK" and computer == "SCISSORS") or \
+    (player == "PAPER" and computer == "ROCK") or \
+    (player == "SCISSORS" and computer == "PAPER") : 
+        print("You Won!")
+    else : 
+        print("Computer Won!")
+
+#replay 
+
+    replay = input("Play Again? (Y/N) : ").upper();
+    if replay == "Y" : 
+        continue
+    elif replay == "N" : 
+        print("Thank You! Game Exited.")
+        break
+    else : 
+        print("Invalid Choice! Exiting the Game.")
+        break
